3ccec0d7cea2de57ba4445be124f42f70ae07713
Derek Hearst <derekallenhearst@gmail.com>
2025-06-26 12:45:49 -0600

    fix: add name attribute to textarea in TextArea component

--- src/lib/TextArea.svelte
+++ src/lib/TextArea.svelte
@@ -24,5 +24,5 @@
 </script>
 
 <Label class={myClass} {label} {name} optional={!required} {disabled}>
-	<textarea bind:this={element} {disabled} class="textarea w-full" {...rest} bind:value></textarea>
+	<textarea bind:this={element} {disabled} {name} class="textarea w-full" {...rest} bind:value></textarea>
 </Label>
